29bfe0fe1631a5c289991a51a4237d4420396a9a
Matt Pollari <mdpollari@gmail.com>
2019-11-06 18:57:41 -0600

    additional work.

--- app2.py
+++ app2.py
@@ -9,8 +9,9 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+#################################################
 # Database Setup
-########
+#################################################
 engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
@@ -23,13 +24,15 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-
+#################################################
 # Flask Setup
-#########
+#################################################
 app = Flask(__name__)
 
+#################################################
 # Flask Routes
-########
+#################################################
+
 @app.route("/")
 def welcome():
     """Welcome to the Hawaii Climate information Site."""
@@ -51,32 +54,7 @@ def precipitation():
         filter(Measurement.date >= query_date).all()
 
     # Dict with date as the key and prcp as the value
-    precp_one = {date: prcp for date, prcp in oneyear_pcp}
-    return jsonify(precp_one)
+    
 
-@app.route("/api/v1.0/stations")
-def stations():
-    """Return a list of stations."""
-    station_list = session.query(Station.station).all()
 
-    # Unravel results into a 1D array and convert to a list
-    stations_flat = list(np.ravel(results))
-    return jsonify(stations_flat)
 
-@app.route("/api/v1.0/tobs")
-def temp_monthly():
-    """Return the temperature observations (tobs) for previous year."""
-    # Calculate the date 1 year ago from last date in database
-    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-
-    # Query the primary station for all tobs from the last year
-    oneyear_temp = session.query(Measurement.tobs).\
-    filter(Measurement.date >= query_date).\
-    filter(Measurement.station == 'USC00519281').\
-    order_by(Measurement.date).all()
-
-    # Unravel results into a 1D array and convert to a list
-    temp_flat = list(np.ravel(oneyear_temp))
-
-    # Return the results
-    return jsonify(temps)
